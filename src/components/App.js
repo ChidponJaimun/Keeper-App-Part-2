cf5b16ddbb0ae6fa67c05e778022bc7b08a3af7a
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-27 04:05:50 +0700

    Final

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import dataList from "../notes";
 import Note from "./Note";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {dataList.map((x) => (
+        <Note key={x.key} title={x.title} content={x.content} />
+      ))}
       <Footer />
     </div>
   );
